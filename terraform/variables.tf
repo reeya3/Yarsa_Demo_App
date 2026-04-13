8c7661a5cf4946377957448ff4e238431e336422
reeya3 <manandharreeya@gmail.com>
2026-04-13 16:45:26 +0545

    updated variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,14 +14,13 @@ variable "instance_type" {
   type = string
 }
 
-variable "access_key" {
-  type = string
-}
+#variable "access_key" {
+#  type = string
+#}
 
-variable "secret_key" {
-  type = string
-}
-
+#variable "secret_key" {
+#  type = string
+#}
 
 variable "image_name" {
   type = string
